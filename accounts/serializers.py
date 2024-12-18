d1d5d3576a6d30b7caef36de5544c10b24ea292e
ERFAN <erfansafarzad7@gmail.com>
2024-12-18 17:21:34 +0330

    edit accounts-app model and serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -199,4 +199,4 @@ class NotificationSerializer(serializers.ModelSerializer):
     """
     class Meta:
         model = Notification
-        fields = ['id', 'user', 'message', 'created_at', 'is_read']
+        fields = ['id', 'user', 'message', 'created_at']
